f984fe44c6d4b34ff6061c7fac277dc7ee8282f8
kovacsbence03 <kovben077@hengersor.hu>
2024-12-09 14:47:54 +0100

    2024.12.09

---
+++ react/reactfilozofia/app.js
@@ -0,0 +1,10 @@
+const btm = document.createElement('button');
+ 
+btm.onclick = function()
+{
+    alert("ez egy natív kód");
+}
+ 
+btm.innerHTML="Natív gomb";
+ 
+document.getElementById("nativ-button-container").appendChild(btm)
