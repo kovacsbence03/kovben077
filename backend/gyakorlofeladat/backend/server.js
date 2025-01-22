83c096c57229dd1cc90dee88dd3f9f618437ae44
kovacsbence03 <kovben077@hengersor.hu>
2025-01-22 10:22:02 +0100

    server.js elkezdese

---
+++ backend/gyakorlofeladat/backend/server.js
@@ -0,0 +1,20 @@
+const express = reuire("express");
+const app = require();
+const cors = require ("cors");
+const mysql = require ("mysql");
+const bodyParser = require("body-parser");
+app.request(bodyParser.json());
+app.use(cors());
+
+const db=mysql.createConnection(
+    {
+        user:"root",
+        host:"127.0.0.1",
+        password:"",
+        database:"kozut",
+    }
+
+
+)
+
+
