3e4063123e7baa5222fa93704a2c51c3bc12ab64
kovacsbence03 <kovben077@hengersor.hu>
2025-01-29 13:07:42 +0100

    uj regiohozzadasa

--- backend/gyakorlofeladat/backend/server.js
+++ backend/gyakorlofeladat/backend/server.js
@@ -1,30 +1,41 @@
 const express = require("express");
-const app = require();
+const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-app.request(bodyParser.json());
-app.use(cors());
+app .use(bodyParser.json());
  
-const db=mysql.createConnection(
-    {
-        user:"root",
-        host:"127.0.0.1",
-        password:"",
-        database:"kozut",
+const db = mysql.createConnection(
+    {      
+     user:"root",
+    host:"127.0.0.1",
+    password:"",
+    database:"kozutak",
     }
 );
  
-app.get("/regiok",(req, res) =>{
-    const sql = "SELECT * FROM `regiok`"
-    db.query(sql,(err, result) =>{
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
+})
+ 
+app.get("/regiok",(req, res) => {
+    const sql = "SELECT * FROM `regiok`";
+    db.query(sql, (err, result) => {
         if(err) return res.json(err);
         return res.json(result)
-    }
-    )
+    })
 })
  
+app.post("/ujregio", (req, res) => {
+    const { Rid, regionev, regio_tipusa } = req.body;
+    const sql = "INSERT INTO `regiok` (`Rid`, `regionev`, `regio_tipusa`) VALUES (?, ?, ?)";
+    const VALUES = ['11', 'Budapest', 'Főváros'];
  
-app.listen(3001,() =>{
-    console.log("Server is running on port 3001");
-})
+    db.query(sql, VALUES, (err, result) => {
+        if (err) return res.json(err);
+        return res.json({ message: "Sikeres beszúrás", result });
+    });
+});
+app.listen(3001, () => {
+    console.log("Server is runningg on port 3001");
+});
